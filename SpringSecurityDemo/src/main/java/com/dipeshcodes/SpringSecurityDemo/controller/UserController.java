29930e118c4b8afd6ed4809a67a2040355c0a224
dipesh <dipeshshrestha0314@gmail.com>
2024-06-24 19:16:40 -0400

    initial commit

---
+++ SpringSecurityDemo/src/main/java/com/dipeshcodes/SpringSecurityDemo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.dipeshcodes.SpringSecurityDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String dashboard(){
+        return "Hello World";
+    }
+}
