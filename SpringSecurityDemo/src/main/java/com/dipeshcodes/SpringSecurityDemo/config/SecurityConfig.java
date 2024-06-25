b47372bcbdec75a48104f266c61b74f9bcdb420f
dipesh <dipeshshrestha0314@gmail.com>
2024-06-24 23:19:04 -0400

    Authenticated Users from DB - Custom Security Configuration

--- SpringSecurityDemo/src/main/java/com/dipeshcodes/SpringSecurityDemo/config/SecurityConfig.java
+++ SpringSecurityDemo/src/main/java/com/dipeshcodes/SpringSecurityDemo/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package com.dipeshcodes.SpringSecurityDemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public AuthenticationProvider authProvider(){
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,19 +50,19 @@ public class SecurityConfig {
 
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.withDefaultPasswordEncoder()
-                                .username("dipesh")
-                                .password("d@123")
-                                .roles("USER")
-                                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                                .username("admin")
-                                .password("admin")
-                                .roles("ADMIN")
-                                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                                .username("dipesh")
+//                                .password("d@123")
+//                                .roles("USER")
+//                                .build();
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                                .username("admin")
+//                                .password("admin")
+//                                .roles("ADMIN")
+//                                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
