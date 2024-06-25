d905f4ba17cc194decc83f97d0ab1eb75b9532df
dipesh <dipeshshrestha0314@gmail.com>
2024-06-24 21:46:39 -0400

    displaying session id

--- SpringSecurityDemo/src/main/java/com/dipeshcodes/SpringSecurityDemo/controller/UserController.java
+++ SpringSecurityDemo/src/main/java/com/dipeshcodes/SpringSecurityDemo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.dipeshcodes.SpringSecurityDemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/")
-    public String dashboard(){
-        return "Hello World";
+    public String dashboard(HttpServletRequest req){
+        System.out.println(req.getSession().getId());
+        return "Hello World the session id is: "+req.getSession().getId();
     }
+
+
 }
