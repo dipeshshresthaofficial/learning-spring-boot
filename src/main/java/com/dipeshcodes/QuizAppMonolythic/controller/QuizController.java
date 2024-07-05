6bdeb7b6da58c29902b9cba256d97827b880cc56
dipesh <dipeshshrestha0314@gmail.com>
2024-07-05 00:49:31 -0400

    retrieving quiz to attempt

--- src/main/java/com/dipeshcodes/QuizAppMonolythic/controller/QuizController.java
+++ src/main/java/com/dipeshcodes/QuizAppMonolythic/controller/QuizController.java
@@ -1,11 +1,13 @@
 package com.dipeshcodes.QuizAppMonolythic.controller;
 
+import com.dipeshcodes.QuizAppMonolythic.model.Question;
+import com.dipeshcodes.QuizAppMonolythic.model.Quiz;
 import com.dipeshcodes.QuizAppMonolythic.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/quiz")
@@ -16,7 +18,13 @@ public class QuizController {
 
     // For admin, who wants to create a new Quiz
     @PostMapping("/create")
-    private String createQuiz(@RequestParam String category, @RequestParam String quizTitle, @RequestParam Integer noOfQue){
+    private ResponseEntity<Quiz> createQuiz(@RequestParam String category, @RequestParam String quizTitle, @RequestParam Integer noOfQue){
         return quizService.createQuiz(category,quizTitle,noOfQue);
+    }
+
+    // When user wants to attempt a quiz
+    @GetMapping("{id}")
+    private ResponseEntity<List<Question>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
     }
 }
--- src/main/java/com/dipeshcodes/QuizAppMonolythic/service/QuizService.java
+++ src/main/java/com/dipeshcodes/QuizAppMonolythic/service/QuizService.java
@@ -5,10 +5,13 @@ import com.dipeshcodes.QuizAppMonolythic.dao.QuizDao;
 import com.dipeshcodes.QuizAppMonolythic.model.Question;
 import com.dipeshcodes.QuizAppMonolythic.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -17,11 +20,24 @@ public class QuizService {
     private QuizDao quizDao;
     @Autowired
     private QuestionDao questionDao;
-    public String createQuiz(String category, String quizTitle, Integer noOfQue) {
+    public ResponseEntity<Quiz> createQuiz(String category, String quizTitle, Integer noOfQue) {
         List<Question> ques = questionDao.getNoOfRandomQuestionsByCategory(noOfQue, category);
         System.out.println(ques);
         Quiz quiz = new Quiz(quizTitle, ques);
         quizDao.save(quiz);
-        return "Success";
+        return new ResponseEntity<>(quiz, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Question>> getQuizQuestions(Integer quizId) {
+        Optional<Quiz> quiz = quizDao.findById(quizId);
+        if(quiz != null){
+            List<Question> questionsFromDB = quiz.get().getQuestions();
+            try{
+                return new ResponseEntity<>(questionsFromDB, HttpStatus.OK);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 }
